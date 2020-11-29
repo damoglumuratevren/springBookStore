20cb9bce33c73f268485ffd7ad37cc8c9044b6b4
damoglumuratevren <60298354+damoglumuratevren@users.noreply.github.com>
2020-11-29 23:49:46 +0300

    First Commit

---
+++ onlinebokkstore/src/main/java/com/mevrend/onlinebokkstore/OnlinebokkstoreApplication.java
@@ -0,0 +1,13 @@
+package com.mevrend.onlinebokkstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlinebokkstoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlinebokkstoreApplication.class, args);
+	}
+
+}
